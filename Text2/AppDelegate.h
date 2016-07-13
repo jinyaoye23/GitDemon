b8ba12fd44bed493f7aab0837c386c288359db4d
yaojinye <jinyaoye23@126.com>
2016-07-13 01:06:34 -0700

    Initial Commit

---
+++ Text2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Text2
+//
+//  Created by yaojinye on 16/7/13.
+//  Copyright (c) 2016年 yaojinye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Text2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Text2
+//
+//  Created by yaojinye on 16/7/13.
+//  Copyright (c) 2016年 yaojinye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
